48e57e53d36b5b2c51875011639b8d802d9e2929
tim <posseslash@gmail.com>
2016-01-11 13:26:18 +0100

    jan 11

---
+++ codeacademy_jquery.js
@@ -0,0 +1,16 @@
+// <script type="text/javascript" src="script.js"></script>
+
+// fadeout action
+
+$(document).ready(function() {
+    $("#green").fadeOut(1000);
+});
+
+
+// mouseenter
+
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $('div').hide();
+    });
+});
